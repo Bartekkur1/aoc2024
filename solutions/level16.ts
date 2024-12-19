064d6c7e090437c3bf9e5f50575e402a249a8418
Bartek Kurpanik <bartekkur1@gmail.com>
2024-12-19 11:31:49 +0100

    Level 16 solved

--- solutions/level16.ts
+++ solutions/level16.ts
@@ -15,6 +15,9 @@ const offsets: Vec[] = [
 type MazeCell = "#" | "." | "S" | "E";
 
 type MazeMap = {
+  setPoint: (target: "S" | "E", pos: Vec) => void;
+  getStart: () => Vec;
+  getEnd: () => Vec;
   set: (pos: Vec, value: string) => void;
   get: (pos: Vec) => MazeCell | undefined;
   has: (pos: Vec) => boolean;
@@ -23,8 +26,19 @@ type MazeMap = {
 
 const createMazeMap = (): MazeMap => {
   const memory: Map<string, MazeCell> = new Map();
+  let start: Vec | null = null;
+  let end: Vec | null = null;
 
   return {
+    setPoint: (target: "S" | "E", pos: Vec) => {
+      if (target === "S") {
+        start = pos;
+      } else {
+        end = pos;
+      }
+    },
+    getStart: () => start!,
+    getEnd: () => end!,
     set: (pos: Vec, value: string) => {
       memory.set(`${pos.x},${pos.y}`, value as MazeCell);
     },
@@ -67,29 +81,6 @@ const applyVec = (pos: Vec, offset: Vec): Vec => {
   };
 };
 
-const vecDiff = (a: Vec, b: Vec): Vec => {
-  return {
-    x: a.x - b.x,
-    y: a.y - b.y,
-  };
-};
-
-const findStart = (maze: MazeMap): Vec => {
-  const [startPosRaw] = maze.memory.entries().find(([_, value]) => value === "S")!;
-  const [x, y] = startPosRaw.split(",").map(Number);
-  return { x, y };
-}
-
-const findEnd = (maze: MazeMap): Vec => {
-  const [endPosRaw] = maze.memory.entries().find(([_, value]) => value === "E")!;
-  const [x, y] = endPosRaw.split(",").map(Number);
-  return { x, y };
-};
-
-const calculateEndDistance = (pos: Vec, end: Vec) => {
-  return Math.abs(pos.x - end.x) + Math.abs(pos.y - end.y);
-};
-
 const readInput = (): Promise<MazeMap> => {
   return readLevel({
     level: 16,
@@ -99,7 +90,12 @@ const readInput = (): Promise<MazeMap> => {
 
       for (let y = 0; y < lines.length; y++) {
         for (let x = 0; x < lines[y].length; x++) {
-          map.set({ x, y }, lines[y][x]);
+          const mapVal = lines[y][x];
+          if (mapVal === 'S' || mapVal === 'E') {
+            map.setPoint(mapVal, { x, y });
+          } else {
+            map.set({ x, y }, lines[y][x]);
+          }
         }
       }
 
@@ -108,123 +104,64 @@ const readInput = (): Promise<MazeMap> => {
   })
 };
 
-const maze = await readInput();
-
-const strToVec = (str: string): Vec => {
-  const [x, y] = str.split(",").map(Number);
-  return { x, y };
-};
-
-const vecEqual = (a: Vec, b: Vec) => {
-  return a.x === b.x && a.y === b.y;
-};
-
-const scoreCache = new Map<string, number>();
-
-const calculateScore = (visited: string[]) => {
-  if (visited.length === 0) return 0;
-
-  if (scoreCache.has(visited.join(","))) {
-    return scoreCache.get(visited.join(","))!;
-  }
-
-  let turns = 0;
-  // Starts facing east
-  let previousPosition = strToVec(visited[0]);
-  let previousDirection = offsets[0];
-
-
-  for (const visit of visited.slice(1)) {
-    const currentPosition = strToVec(visit);
-    const direction = vecDiff(currentPosition, previousPosition);
-
-    if (!vecEqual(direction, previousDirection)) {
-      turns += 1;
-    }
-    previousPosition = currentPosition;
-    previousDirection = direction;
-  }
-
-  const score = turns * 1000 + visited.length - 1;
-  scoreCache.set(visited.join(","), score);
-  return score;
-};
-
-const traverseMazeForBestPath = (maze: MazeMap) => {
-  const start = findStart(maze);
-  const openSet: { pos: Vec; path: string[]; score: number }[] = [
-    { pos: start, path: [], score: 0 },
+const traverseMaze = (maze: MazeMap) => {
+  const SPLITTER = '->';
+  let bestScore = Infinity;
+  const end = maze.getEnd();
+  const scoreMap: Record<string, number> = {};
+  const queue: { pos: Vec, direction: Vec, score: number, path: string }[] = [
+    { pos: maze.getStart(), direction: offsets[0], score: 0, path: '' },
   ];
-  const bestScores = new Map<string, number>();
+  const pathsScore: [string, number][] = [];
 
-  while (openSet.length) {
-    openSet.sort((a, b) => a.score - b.score);
-    const { pos, path, score } = openSet.shift()!;
+  while (queue.length > 0) {
+    const { direction, pos, score, path } = queue.shift()!;
+
     const posKey = `${pos.x},${pos.y}`;
+    const scoreKey = `${posKey},${direction.x},${direction.y}`;
 
-    if (bestScores.has(posKey) && bestScores.get(posKey)! <= score) continue;
-    bestScores.set(posKey, score);
+    if (maze.get(pos) === "#" || scoreMap[scoreKey] < score) {
+      continue;
+    }
 
-    if (maze.get(pos) === "E") return { path: [...path, posKey], score: score + 1 };
+    const extendedPath = path + posKey + SPLITTER;
+    scoreMap[scoreKey] = score;
 
-    for (const nextPos of offsets.map(o => applyVec(pos, o)).filter(p => maze.get(p) !== "#")) {
-      const nextPosKey = `${nextPos.x},${nextPos.y}`;
-      const newPath = [...path, posKey];
-      const newScore = calculateScore(newPath);
-
-      if (!bestScores.has(nextPosKey) || bestScores.get(nextPosKey)! >= newScore) {
-        openSet.push({ pos: nextPos, path: newPath, score: newScore });
+    if (end.x === pos.x && end.y === pos.y) {
+      pathsScore.push([extendedPath, score]);
+      if (score < bestScore) {
+        bestScore = score;
       }
+      continue;
+    }
+
+    for (const move of offsets) {
+      const newPos = applyVec(pos, move);
+      if (maze.get(newPos) === "#" ||
+        (newPos.x === pos.x && newPos.y === pos.y)) {
+        continue;
+      }
+      const nextScore = score + (direction === move ? 1 : 1001);
+      queue.push({ pos: newPos, direction: move, score: nextScore, path: extendedPath });
     }
   }
-  return null;
+
+  const uniqueSeats: Set<string> = new Set();
+  const bestPaths = pathsScore.filter((path) => path[1] === bestScore);
+
+  for (const [path] of bestPaths) {
+    const seats = path.split("->");
+    for (const seat of seats) {
+      if (seat === "") {
+        continue;
+      }
+      uniqueSeats.add(seat);
+    }
+  }
+
+  console.log(`Best score is: ${bestScore}`);
+  console.log(`Unique seats: ${uniqueSeats.size}`);
 };
 
-const traverseMaze = (maze: MazeMap, bestScore: number) => {
-  const start = findStart(maze);
-  const finishingPaths: string[][] = [];
-  let iterations = 0;
-
-  const visitStack: { pos: Vec; visited: string[] }[] = [
-    { pos: start, visited: [] },
-  ];
-
-  while (visitStack.length > 0) {
-    iterations += 1;
-
-    if (iterations % 100000 === 0) {
-      console.log(visitStack.length);
-    }
-
-    const { pos, visited } = visitStack.pop()!;
-    const currentScore = calculateScore(visited || []);
-    if (currentScore > bestScore) continue;
-
-    if (!maze.has(pos) || visited.includes(`${pos.x},${pos.y}`)) {
-      continue;
-    } else {
-      visited.push(`${pos.x},${pos.y}`);
-    }
-
-    if (maze.get(pos) === "E") {
-      finishingPaths.push(visited);
-      continue;
-    }
-
-    const possibleMovements = offsets
-      .map(offset => applyVec(pos, offset))
-      .filter(nextPos => maze.get(nextPos) !== "#")
-
-    for (let possibleMovement of possibleMovements) {
-      visitStack.push({ pos: possibleMovement, visited: Array.from(visited) });
-    }
-
-  };
-  console.log(finishingPaths)
-}
-
-const res = traverseMazeForBestPath(maze);
-console.log(res);
-traverseMaze(maze, res!.score);
-// drawMaze(maze, res!.path);
-// console.log(res);
+const maze = await readInput();
+traverseMaze(maze);
